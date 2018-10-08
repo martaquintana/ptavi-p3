b4f8429339eaf698ffdfe292b764c6a6f534ef60
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-10-08 12:32:29 +0200

    He estado probando

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class SmallSMILHandler(ContentHandler):
+    def __init__ (self):
+     
+        
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    #cHandler.imprimir(cHandler)
+    
