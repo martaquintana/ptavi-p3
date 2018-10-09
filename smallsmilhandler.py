f4280c8c40c52829161c14378286ab73adf0e9d1
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-10-09 20:26:32 +0200

    Coge los valores que estan dentro de root-layout y los imprime

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,8 +6,24 @@ from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
     def __init__ (self):
-     
+      self.width = ""
+      self.height = ""
+      self.background_color= ""
+
+    def startElement(self, name, attrs):
+        """
+        Método que se llama cuando se abre una etiqueta
+        """
+        if name == 'root-layout':
+            # De esta manera tomamos los valores de los atributos
+            self.width = attrs.get('width', "")
+            self.height = attrs.get('height', "")
+            self.background_color = attrs.get('background-color', "")
+            print(self.width)
+            print(self.height)
+            print(self.background_color)
         
+
 if __name__ == "__main__":
     """
     Programa principal
