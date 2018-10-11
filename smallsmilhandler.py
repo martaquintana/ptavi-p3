7205d35ff28f08823c192a7ea358c58b1e90ac66
martaquintana <Marta@MacBook-Pro-de-Marta-2.local>
2018-10-11 20:27:11 +0200

    Mete en un dicionario  todas las etiquetas con sus atributos

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,26 +7,24 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
     def __init__ (self):
       
-      self.diccionario= {"root-layout": ['width', 'height',  'background-color']} 
+      self.diccionario= {"root-layout": ['width', 'height',  'background-color'], 
+                         "region":['id','top','left'], 
+                         "img":['src','region','begin','dur','end'],
+                         "audio":['src','begin'],
+                         "textstream":['src','region','fill']} 
       self.tag_list=[]
-      
-      print(self.diccionario.values())
-      
       self.etiquetas=self.diccionario.keys()
-      print(self.etiquetas)
+ 
 
     def startElement(self, name, attrs):
         
         dic_tag={}
-        if name in self.diccionario:
+        if name in self.etiquetas:
             dic_tag['etiqueta'] = name
             # De esta manera tomamos los valores de los atributos
             for  atributo in self.diccionario[name]:
                 dic_tag[atributo] = attrs.get(atributo, "")
-                self.tag_list.append(dic_tag[atributo])
-        print(self.tag_list)
-        
-
+       
 if __name__ == "__main__":
     """
     Programa principal
@@ -35,5 +33,5 @@ if __name__ == "__main__":
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
-    #cHandler.imprimir(cHandler)
-    
+   
+
