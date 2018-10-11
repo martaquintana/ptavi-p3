0eef64d90aff9769df054313fdd4fce3d8ad3f53
martaquintana <Marta@MacBook-Pro-de-Marta-2.local>
2018-10-11 20:48:06 +0200

    Funciona
    Devuelve la lista de etiquetas, sus atributos y el contenido de los atributos.

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,27 +4,39 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
-    def __init__ (self):
-      
-      self.diccionario= {"root-layout": ['width', 'height',  'background-color'], 
-                         "region":['id','top','left'], 
-                         "img":['src','region','begin','dur','end'],
-                         "audio":['src','begin'],
-                         "textstream":['src','region','fill']} 
-      self.tag_list=[]
-      self.etiquetas=self.diccionario.keys()
- 
+
+    def __init__(self):
+
+        self.diccionario = {
+            "root-layout": ['width', 'height', 'background-color'],
+            "region": ['id', 'top', 'left'],
+            "img": ['src', 'region', 'begin', 'dur', 'end'],
+            "audio": ['src', 'begin'],
+            "textstream": ['src', 'region', 'fill']
+            }
+        self.tag_list = []
+        self.etiquetas = self.diccionario.keys()
 
     def startElement(self, name, attrs):
-        
-        dic_tag={}
+
+        dic_tag = {}
         if name in self.etiquetas:
             dic_tag['etiqueta'] = name
             # De esta manera tomamos los valores de los atributos
-            for  atributo in self.diccionario[name]:
+            for atributo in self.diccionario[name]:
                 dic_tag[atributo] = attrs.get(atributo, "")
-       
+            self.tag_list.append(dic_tag)
+
+    def get_tags(self):
+        """
+        Devuelve la lista de etiquetas,
+        sus atributos y el contenido de los atributos
+        """
+        return self.tag_list
+
+
 if __name__ == "__main__":
     """
     Programa principal
@@ -33,5 +45,4 @@ if __name__ == "__main__":
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
-   
-
+    cHandler.get_tags()
