79f554f2a4aef1c81aa9e696182d01410806ad89
martaquintana <Marta@MacBook-Pro-de-Marta-2.local>
2018-10-14 12:34:41 +0200

    Descarga los enlaces http: si pongo urlretrieve me da error

--- karaoke.py
+++ karaoke.py
@@ -6,12 +6,14 @@ import json
 import smallsmilhandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+from urllib.request import urlretrieve
 
 
 class KaraokeLocal:
     
     def __init__(self,fichero):
         self.get_tags=[]
+        self.dicc={}
    
     def __str__(self):
         """
@@ -20,11 +22,11 @@ class KaraokeLocal:
         self.get_tags=cHandler.get_tags()
         #print(self.get_tags)
         for linea in range(0,len (self.get_tags)):
-            dicc=self.get_tags[linea]
+            self.dicc=self.get_tags[linea]
             #print(dicc)
             #print ('{etiqueta}\t{width}\t{height}\t{background-color}\n'.format(**dicc))
             
-            for clave, valor in dicc.items():
+            for clave, valor in self.dicc.items():
                 if clave == 'etiqueta' :
                     print('{v}\t'.format(v=valor),end='\t')
                 elif valor == '':
@@ -40,7 +42,30 @@ class KaraokeLocal:
             fichero_json = fichero.split('.')[0]+'.json'
         json.dump(self.get_tags,open(fichero_json,"w"))
         
-
+    def do_local(self):
+        """
+        Descarga en local el contenido multimedia
+        """
+        for linea in range(0,len (self.get_tags)):
+            self.dicc=self.get_tags[linea]
+            for clave, valor in self.dicc.items():
+                if clave == 'src' and valor[0:5] == "http:":
+                    newvalor=valor.split('/')[-1] 
+                    #urlretrieve(valor, newvalor)
+                    #No me funciona en casa si lo quito si lo pone bien
+                    valor=newvalor
+                
+                if clave == 'etiqueta' :
+                    print('{v}\t'.format(v=valor),end='\t')
+                elif valor == '':
+                    print('')
+                else:
+                    print ('{c}="{v}"'.format(c=clave,v=valor),end='\t')
+            print('\n')
+        
+                        
+                        
+                   
 if __name__ == '__main__':
     fichero = sys.argv[1]
     try:
@@ -51,6 +76,7 @@ if __name__ == '__main__':
         parser.parse(open(fichero))
         KaraokeLocal.__str__(cHandler)
         KaraokeLocal.to_json(cHandler,fichero)
+        KaraokeLocal.do_local(cHandler)
 
         
     except(FileNotFoundError,IndexError):
