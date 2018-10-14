7c5f89b8f65acbc8adb236bd8f35b75abeec0715
martaquintana <Marta@MacBook-Pro-de-Marta-2.local>
2018-10-14 11:12:33 +0200

    imprime correctamente, ahora hacer parte json

--- karaoke.py
+++ karaoke.py
@@ -2,25 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
 import smallsmilhandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-class Karaoke:
+class KaraokeLocal:
     get_tags=[]
    
-    def imprimir(self):
+    def __str__(self):
         get_tags=cHandler.get_tags()
         print(get_tags)
         for linea in range(0,len (get_tags)):
             dicc=get_tags[linea]
-            print(dicc)
+            #print(dicc)
             #print ('{etiqueta}\t{width}\t{height}\t{background-color}\n'.format(**dicc))
-  
-            for clave, valor in dicc.items(): 
-                print ('{c}={v}'.format(c=clave,v=valor),end='\t')
+            
+            for clave, valor in dicc.items():
+                if clave == 'etiqueta' :
+                    print('{v}\t'.format(v=valor),end='\t')
+                elif valor == '':
+                    print('')
+                else:
+                    print ('{c}="{v}"'.format(c=clave,v=valor),end='\t')
             print('\n')
+    def to_json(self,fichero)
 
 
 if __name__ == '__main__':
@@ -31,7 +38,7 @@ if __name__ == '__main__':
         parser = make_parser()
         parser.setContentHandler(cHandler)
         parser.parse(open(fichero))
-        Karaoke.imprimir(cHandler)
+        KaraokeLocal.__str__(cHandler)
 
         
     except(FileNotFoundError,IndexError):
